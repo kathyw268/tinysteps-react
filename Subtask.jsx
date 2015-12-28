b0719a45be2a1906dea6b4456c99111976d4bd06
Kathy Wu <Kathy.Wu@deepfocus.net>
2015-12-27 21:13:03 -0500

    modified meteor tutorial

---
+++ Subtask.jsx
@@ -0,0 +1,17 @@
+// Task component - represents a single todo item
+Subtask = React.createClass({
+
+
+  render() {
+    // Give tasks a different className when they are checked off,
+    // so that we can style them nicely in CSS
+    // const subtaskClassName = (this.props.task.checked ? "checked" : "");
+    console.log(this);
+    return (
+      <li className="blah">
+
+      </li>
+
+    );
+  }
+});
